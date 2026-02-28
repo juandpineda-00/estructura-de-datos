0918c425bc5d299c1ae0e7c242d985ddad80a003
00jdaH <juand.pineda@autonoma.edu.co>
2026-02-27 19:34:38 -0500

    prueba de actualizacion de datos

---
+++ clase.py
@@ -0,0 +1,15 @@
+a=[1]
+b={}
+c=()
+
+print(type(a),type(b),type(c))  
+
+
+print("hols jobo")
+print("hols jobo")
+print("hols jobo")
+print("hols jobo")
+print("hols jobo")
+print("hols jobo")
+print("hols jobo")
+
